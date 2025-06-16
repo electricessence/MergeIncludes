1f9320168309636286ab71ba30e09000d1445365
electricessence <5899455+electricessence@users.noreply.github.com>
2025-06-16 08:12:01 -0700

    Enhance LinkableTextPath with new constructors and styles
    Updated the LinkableTextPath class to improve constructor overloads and add XML documentation. Introduced methods for setting styles on path components and enhanced Measure and Render methods for clickable links in Windows Terminal. Added a new method to create linked segments and improved the readability of the IsPathSeparator method.

--- Spectre.Console.Extensions/LinkableTextPath.cs
+++ Spectre.Console.Extensions/LinkableTextPath.cs
@@ -1,4 +1,3 @@
-using Spectre.Console;
 using Spectre.Console.Rendering;
 
 namespace Spectre.Console.Extensions;
@@ -41,252 +40,239 @@ namespace Spectre.Console.Extensions;
 ///    console.Write(PathLink.Smart("/path/to/file.cs")); // Uses extension-specific styling
 ///    </code>
 /// </remarks>
-public sealed class LinkableTextPath : IRenderable
+/// <remarks>
+/// Creates a new instance of <see cref="LinkableTextPath"/> with a link URL.
+/// </remarks>
+/// <param name="path">The path.</param>
+/// <param name="linkUrl">The URL to link to.</param>
+public sealed class LinkableTextPath(string path, string? linkUrl) : IRenderable
 {
-    private readonly string _path;
-    private readonly string? _linkUrl;
-    private Style? _rootStyle;
-    private Style? _separatorStyle;
-    private Style? _stemStyle;
-    private Style? _leafStyle;
+	private readonly string _path = path ?? throw new ArgumentNullException(nameof(path));
+	private Style? _rootStyle;
+	private Style? _separatorStyle;
+	private Style? _stemStyle;
+	private Style? _leafStyle;
 
-    /// <summary>
-    /// Gets a value indicating whether we're running in Windows Terminal.
-    /// </summary>
-    /// <remarks>
-    /// Windows Terminal sets the WT_SESSION environment variable when it runs.
-    /// Only Windows Terminal and some other modern terminals support clickable links.
-    /// </remarks>
-    private static bool IsWindowsTerminal => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WT_SESSION"));
+	/// <summary>
+	/// Gets a value indicating whether we're running in Windows Terminal.
+	/// </summary>
+	/// <remarks>
+	/// Windows Terminal sets the WT_SESSION environment variable when it runs.
+	/// Only Windows Terminal and some other modern terminals support clickable links.
+	/// </remarks>
+	private static bool IsWindowsTerminal => !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WT_SESSION"));
 
-    /// <summary>
-    /// Creates a new instance of <see cref="LinkableTextPath"/>.
-    /// </summary>
-    /// <param name="path">The path.</param>
-    public LinkableTextPath(string path)
-        : this(path, null)
-    {
-    }
+	/// <summary>
+	/// Creates a new instance of <see cref="LinkableTextPath"/>.
+	/// </summary>
+	/// <param name="path">The path.</param>
+	public LinkableTextPath(string path)
+		: this(path, null)
+	{
+	}
 
-    /// <summary>
-    /// Creates a new instance of <see cref="LinkableTextPath"/> with a link URL.
-    /// </summary>
-    /// <param name="path">The path.</param>
-    /// <param name="linkUrl">The URL to link to.</param>
-    public LinkableTextPath(string path, string? linkUrl)
-    {
-        _path = path ?? throw new ArgumentNullException(nameof(path));
-        _linkUrl = linkUrl;
-    }
+	/// <summary>
+	/// Creates a new instance of <see cref="LinkableTextPath"/> with a link URL.
+	/// </summary>
+	/// <param name="path">The path.</param>
+	/// <param name="linkToSelf">If true, links to the path itself.</param>
+	public LinkableTextPath(string path, bool linkToSelf)
+		: this(path, linkToSelf ? path : null)
+	{
+	}
 
-    /// <summary>
-    /// Creates a new instance of <see cref="LinkableTextPath"/> with a link URL.
-    /// </summary>
-    /// <param name="path">The path.</param>
-    /// <param name="linkToSelf">If true, links to the path itself.</param>
-    public LinkableTextPath(string path, bool linkToSelf)
-        : this(path, linkToSelf ? path : null)
-    {
-    }
+	/// <summary>
+	/// Sets the style of the path's root.
+	/// </summary>
+	/// <param name="style">The root style.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath RootStyle(Style style)
+	{
+		_rootStyle = style;
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of the path's root.
-    /// </summary>
-    /// <param name="style">The root style.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath RootStyle(Style style)
-    {
-        _rootStyle = style;
-        return this;
-    }
+	/// <summary>
+	/// Sets the style of the path's root.
+	/// </summary>
+	/// <param name="color">The root color.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath RootStyle(Color color)
+	{
+		_rootStyle = new Style(foreground: color);
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of the path's root.
-    /// </summary>
-    /// <param name="color">The root color.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath RootStyle(Color color)
-    {
-        _rootStyle = new Style(foreground: color);
-        return this;
-    }
+	/// <summary>
+	/// Sets the style of path separators.
+	/// </summary>
+	/// <param name="style">The separator style.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath SeparatorStyle(Style style)
+	{
+		_separatorStyle = style;
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of path separators.
-    /// </summary>
-    /// <param name="style">The separator style.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath SeparatorStyle(Style style)
-    {
-        _separatorStyle = style;
-        return this;
-    }
+	/// <summary>
+	/// Sets the style of path separators.
+	/// </summary>
+	/// <param name="color">The separator color.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath SeparatorStyle(Color color)
+	{
+		_separatorStyle = new Style(foreground: color);
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of path separators.
-    /// </summary>
-    /// <param name="color">The separator color.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath SeparatorStyle(Color color)
-    {
-        _separatorStyle = new Style(foreground: color);
-        return this;
-    }
+	/// <summary>
+	/// Sets the style of inner path segments.
+	/// </summary>
+	/// <param name="style">The stem style.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath StemStyle(Style style)
+	{
+		_stemStyle = style;
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of inner path segments.
-    /// </summary>
-    /// <param name="style">The stem style.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath StemStyle(Style style)
-    {
-        _stemStyle = style;
-        return this;
-    }
+	/// <summary>
+	/// Sets the style of inner path segments.
+	/// </summary>
+	/// <param name="color">The stem color.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath StemStyle(Color color)
+	{
+		_stemStyle = new Style(foreground: color);
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of inner path segments.
-    /// </summary>
-    /// <param name="color">The stem color.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath StemStyle(Color color)
-    {
-        _stemStyle = new Style(foreground: color);
-        return this;
-    }
+	/// <summary>
+	/// Sets the style of the last path segment.
+	/// </summary>
+	/// <param name="style">The leaf style.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath LeafStyle(Style style)
+	{
+		_leafStyle = style;
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of the last path segment.
-    /// </summary>
-    /// <param name="style">The leaf style.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath LeafStyle(Style style)
-    {
-        _leafStyle = style;
-        return this;
-    }
+	/// <summary>
+	/// Sets the style of the last path segment.
+	/// </summary>
+	/// <param name="color">The leaf color.</param>
+	/// <returns>The same instance so that multiple calls can be chained.</returns>
+	public LinkableTextPath LeafStyle(Color color)
+	{
+		_leafStyle = new Style(foreground: color);
+		return this;
+	}
 
-    /// <summary>
-    /// Sets the style of the last path segment.
-    /// </summary>
-    /// <param name="color">The leaf color.</param>
-    /// <returns>The same instance so that multiple calls can be chained.</returns>
-    public LinkableTextPath LeafStyle(Color color)
-    {
-        _leafStyle = new Style(foreground: color);
-        return this;
-    }
+	/// <inheritdoc/>
+	public Measurement Measure(RenderOptions options, int maxWidth)
+	{
+		// Create a TextPath and apply our styles, then use its measurement
+		var textPath = new TextPath(_path);
 
-    /// <inheritdoc/>
-    public Measurement Measure(RenderOptions options, int maxWidth)
-    {
-        // Create a TextPath and apply our styles, then use its measurement
-        var textPath = new TextPath(_path);
+		if (_rootStyle != null) textPath.RootStyle(_rootStyle);
+		if (_separatorStyle != null) textPath.SeparatorStyle(_separatorStyle);
+		if (_stemStyle != null) textPath.StemStyle(_stemStyle);
+		if (_leafStyle != null) textPath.LeafStyle(_leafStyle);
 
-        if (_rootStyle != null) textPath.RootStyle(_rootStyle);
-        if (_separatorStyle != null) textPath.SeparatorStyle(_separatorStyle);
-        if (_stemStyle != null) textPath.StemStyle(_stemStyle);
-        if (_leafStyle != null) textPath.LeafStyle(_leafStyle);
-        
-        return textPath.Measure(options, maxWidth);
-    }
-    
-    /// <inheritdoc/>
-    public IEnumerable<Segment> Render(RenderOptions options, int maxWidth)
-    {
-        // Create a TextPath and apply our styles
-        var textPath = new TextPath(_path);
+		return textPath.Measure(options, maxWidth);
+	}
 
-        if (_rootStyle != null) textPath.RootStyle(_rootStyle);
-        if (_separatorStyle != null) textPath.SeparatorStyle(_separatorStyle);
-        if (_stemStyle != null) textPath.StemStyle(_stemStyle);
-        if (_leafStyle != null) textPath.LeafStyle(_leafStyle);
+	/// <inheritdoc/>
+	public IEnumerable<Segment> Render(RenderOptions options, int maxWidth)
+	{
+		// Create a TextPath and apply our styles
+		var textPath = new TextPath(_path);
 
-        // If no link URL is specified or not in Windows Terminal, just render the text path without links
-        if (string.IsNullOrEmpty(_linkUrl) || !IsWindowsTerminal)
-        {
-            return textPath.Render(options, maxWidth);
-        }
+		if (_rootStyle != null) textPath.RootStyle(_rootStyle);
+		if (_separatorStyle != null) textPath.SeparatorStyle(_separatorStyle);
+		if (_stemStyle != null) textPath.StemStyle(_stemStyle);
+		if (_leafStyle != null) textPath.LeafStyle(_leafStyle);
 
-        // We are in Windows Terminal and have a link URL, so add links to the segments
-        var segments = textPath.Render(options, maxWidth).ToList();
-        return CreateLinkedSegments(segments);
-    }
+		// If no link URL is specified or not in Windows Terminal, just render the text path without links
+		if (string.IsNullOrEmpty(linkUrl) || !IsWindowsTerminal)
+		{
+			return textPath.Render(options, maxWidth);
+		}
 
-    /// <summary>
-    /// Creates linked segments for a path, with each folder component linking to its corresponding path
-    /// </summary>
-    private IEnumerable<Segment> CreateLinkedSegments(List<Segment> segments)
-    {
-        var pathBuilder = new List<string>();
-        var isFirstSegment = true;
+		// We are in Windows Terminal and have a link URL, so add links to the segments
+		var segments = textPath.Render(options, maxWidth).ToList();
+		return CreateLinkedSegments(segments);
+	}
 
-        foreach (var segment in segments)
-        {
-            // Pass through control codes unchanged
-            if (segment.IsControlCode || string.IsNullOrEmpty(segment.Text))
-            {
-                yield return segment;
-                continue;
-            }
+	/// <summary>
+	/// Creates linked segments for a path, with each folder component linking to its corresponding path
+	/// </summary>
+	private IEnumerable<Segment> CreateLinkedSegments(List<Segment> segments)
+	{
+		var pathBuilder = new List<string>();
+		var isFirstSegment = true;
 
-            // Path separators don't get links
-            if (IsPathSeparator(segment.Text))
-            {
-                yield return segment;
-                continue;
-            }
+		foreach (var segment in segments)
+		{
+			// Pass through control codes unchanged
+			if (segment.IsControlCode || string.IsNullOrEmpty(segment.Text))
+			{
+				yield return segment;
+				continue;
+			}
 
-            // This is a path component (folder or file name)
-            pathBuilder.Add(segment.Text);
+			// Path separators don't get links
+			if (IsPathSeparator(segment.Text))
+			{
+				yield return segment;
+				continue;
+			}
 
-            // Determine the link URL for this segment
-            string segmentLinkUrl;
-            
-            // Special handling for the first segment of absolute paths
-            if (isFirstSegment && Path.IsPathRooted(_path))
-            {
-                if (segment.Text.EndsWith(":")) // Windows drive (C:)
-                {
-                    segmentLinkUrl = segment.Text + Path.DirectorySeparatorChar;
-                }
-                else
-                {
-                    segmentLinkUrl = Path.DirectorySeparatorChar + segment.Text;
-                }
-                
-                isFirstSegment = false;
-            }
-            else
-            {
-                // Build cumulative path for this segment
-                segmentLinkUrl = isFirstSegment
-                    ? segment.Text
-                    : string.Join(Path.DirectorySeparatorChar.ToString(), pathBuilder);
-                
-                isFirstSegment = false;
-            }
-            
-            // Create a new style that includes the link
-            var style = segment.Style != Style.Plain
-                ? new Style(
-                    segment.Style.Foreground,
-                    segment.Style.Background,
-                    segment.Style.Decoration,
-                    link: segmentLinkUrl)
-                : new Style(link: segmentLinkUrl);
+			// This is a path component (folder or file name)
+			pathBuilder.Add(segment.Text);
 
-            yield return new Segment(segment.Text, style);
-        }
-    }
+			// Determine the link URL for this segment
+			string segmentLinkUrl;
 
-    /// <summary>
-    /// Checks if the given text is a path separator.
-    /// </summary>
-    private static bool IsPathSeparator(string text)
-    {
-        return text == "/" || text == "\\" || 
-               text == Path.DirectorySeparatorChar.ToString() || 
-               text == Path.AltDirectorySeparatorChar.ToString();
-    }
+			// Special handling for the first segment of absolute paths
+			if (isFirstSegment && Path.IsPathRooted(_path))
+			{
+				segmentLinkUrl = segment.Text.EndsWith(':')
+					? segment.Text + Path.DirectorySeparatorChar
+					: Path.DirectorySeparatorChar + segment.Text;
+
+			}
+			else
+			{
+				// Build cumulative path for this segment
+				segmentLinkUrl = isFirstSegment
+					? segment.Text
+					: string.Join(Path.DirectorySeparatorChar.ToString(), pathBuilder);
+			}
+
+			isFirstSegment = false;
+
+			// Create a new style that includes the link
+			var style = segment.Style != Style.Plain
+				? new Style(
+					segment.Style.Foreground,
+					segment.Style.Background,
+					segment.Style.Decoration,
+					link: segmentLinkUrl)
+				: new Style(link: segmentLinkUrl);
+
+			yield return new Segment(segment.Text, style);
+		}
+	}
+
+	/// <summary>
+	/// Checks if the given text is a path separator.
+	/// </summary>
+	private static bool IsPathSeparator(string text)
+	{
+		return text == "/" || text == "\\" ||
+			   text == Path.DirectorySeparatorChar.ToString() ||
+			   text == Path.AltDirectorySeparatorChar.ToString();
+	}
 }
